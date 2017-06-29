4a9a7be655a54d28e5ed3962699917403aeb8bd6
Jess Poemape <jesspoemape@gmail.com>
2017-06-29 13:20:09 -0600

    config file has a secret

--- server/server.js
+++ server/server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cors = require('cors');
+const config = require('./config');
 
 const app = express();
+
+// cors whitelist
+var corsOptions = {
+    origin: 'http://localhost:3000'
+}
+
 app.use(bodyParser.json());
+app.use(cors(corsOptions)); // this enables CORS across all origins based on the specified whitelist origins 
+app.use(session({
+    secret: config.sessionSecret
+}));
+
+
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
